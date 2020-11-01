0ff8638ec64d111ee55b76a8bb48dcb0156640ac
JonoMacC <39803935+JonoMacC@users.noreply.github.com>
2020-10-31 20:10:31 -0400

    Change node dependencies to dev Correct router base path condition

--- src/util/routerBasePath.js
+++ src/util/routerBasePath.js
@@ -1,4 +1,4 @@
 const routerBasePath =
-  process.env.REACT_APP_NETLIFY === true ? `/.netlify/functions/api` : `/api`;
+  process.env.REACT_APP_NETLIFY === "true" ? `/.netlify/functions/api` : `/api`;
 
 export default routerBasePath;
