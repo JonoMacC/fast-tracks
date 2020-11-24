2f64b8c5ab231aa975843d581d98f367fa7ca5ce
JonoMacC <39803935+JonoMacC@users.noreply.github.com>
2020-11-23 23:04:07 -0500

    Unregister service worker Remove webVitals reporting

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ThemeProvider from "./contexts/ThemeContext";
 import AppProvider from "./contexts/AppContext";
 import App from "./Components/Routes";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -26,9 +26,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
